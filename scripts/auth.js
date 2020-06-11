06f250f4be7e82a08dec2adb40916e4e5aeba05d
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-06-11 18:06:46 +0900

    一通りのAUth

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,7 @@
+//get data
+db.collection('guides').get().then(snapshot=>{
+   setUpGuides(snapshot.docs)
+})
 //dignup
 const signupform = document.querySelector('#signup-form');
 signupform.addEventListener('submit',(e)=>{
@@ -12,3 +16,38 @@ signupform.addEventListener('submit',(e)=>{
         signupform.reset()
     })
 }) 
+
+//logout
+const logout =document.querySelector('#logout')
+logout.addEventListener('click', (e)=>{
+    e.preventDefault();
+    auth.signOut().then(() =>{
+        console.log('user sined out')
+    }).catch(err=>{
+        alert(err)
+    })
+})
+
+//login
+const loginForm =document.querySelector('#login-form');
+loginForm.addEventListener('submit' , (e)=>{
+    e.preventDefault();
+    const email=loginForm['login-email'].value;
+    const password=loginForm['login-password'].value;
+    auth.signInWithEmailAndPassword(email,password).then(cred=>{
+   
+      //close the login modal and reset the form
+      const modal = document.querySelector("#modal-login")
+        M.Modal.getInstance(modal).close();
+        signupform.reset()
+    })
+})
+auth.onAuthStateChanged(function (user) {
+    if(user){
+        console.log('user logged in:',user)
+    }else{
+        console.log("user logout")
+    }
+});
+
+
