7f0932593e28e196101a0cbf271591ecc4cd300a
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-06-21 14:15:06 +0900

    自己紹介文の追加

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,19 +1,42 @@
 
 //dignup
-const signupform = document.querySelector('#signup-form');
-signupform.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    const email = signupform['signup-email'].value;   
-    const password = signupform['signup-password'].value;
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
 
-    auth.createUserWithEmailAndPassword(email,password).then(cred=>{
-        console.log(cred.user)
-        const modal = document.querySelector("#modal-signup")
+  // sign up the user & add firestore data
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return db.collection('users').doc(cred.user.uid).set({
+      bio: signupForm['signup-bio'].value
+    });
+  }).then(() => {
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+//create new guide
+const createForm = document.querySelector("#create-form")
+createForm.addEventListener('submit',(e) =>{
+    e.preventDefault();
+
+    db.collection("guides").add({
+        title:createForm['title'].value,
+        content:createForm['content'].value
+    }).then(()=>{
+        const modal = document.querySelector("#modal-content")
         M.Modal.getInstance(modal).close();
-        signupform.reset()
+        createForm.reset()
+    }).catch(err=>{
+        console.log(err.message)
     })
-}) 
-
+})
 //logout
 const logout =document.querySelector('#logout')
 logout.addEventListener('click', (e)=>{
@@ -43,10 +66,12 @@ auth.onAuthStateChanged(function (user) {
     if(user){
    //get data
    alert("認証成功！")
-    db.collection('guides').get().then(snapshot=>{
+    db.collection('guides').onSnapshot(snapshot=>{
     setUpGuides(snapshot.docs)
     setupUI(user)
- })
+ },err=>{
+     console.log(err.message)
+ });
     }else{
         setupUI()
        setUpGuides([])
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,22 @@
 const guideList = document.querySelector('.guides')
 const loggedOutLinks = document.querySelectorAll('.logged-out')
 const loggedInLinks = document.querySelectorAll('.logged-in')
+const accountDetails = document.querySelector('.account-details')
 const setupUI = (user) =>{
   if(user){
+      db.collection("users").doc(user.uid).get().then(doc=>{
+        const html=`
+        <div>Looged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+      `
+      accountDetails.innerHTML=html
+      })
+
     //toggle UI elements
     loggedInLinks.forEach(item => item.style.display = 'block')
     loggedOutLinks.forEach(item =>item.style.display = 'none')
   } else{
+    accountDetails.innerHTML=""
     loggedInLinks.forEach(item => item.style.display = 'none')
     loggedOutLinks.forEach(item =>item.style.display = 'block')
   }
